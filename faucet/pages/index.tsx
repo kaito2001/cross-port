39c6b7c39e26b91baeb2c2c2cd42a76f458c5e0c
bakansm <khuynhbao5@gmail.com>
2023-07-31 01:27:56 +0700

    Add faucet api

--- faucet/pages/index.tsx
+++ faucet/pages/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 
 export default function Home() {
@@ -5,6 +6,20 @@ export default function Home() {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: `http://34.232.46.225:9001/faucet/${walletAddress}`,
+    };
+        
+    axios.request(config)
+      .then((response) => {
+        alert(response.data.message);
+      })
+      .catch((error) => {
+        alert(error);
+      });
   }
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setWalletAddress(event.target.value)
@@ -25,19 +40,20 @@ export default function Home() {
       </div>
       <div className="divide-x divide-dashed divide-gray-300"></div>
       <div className="flex flex-col space-y-4">
-        <form onSubmit={handleSubmit} className=" w-full">
+        <form onSubmit={handleSubmit} className="w-full space-y-4">
           <input
             type="text"
             name="walletAddress"
             placeholder="Enter your CrossTech Address"
             className=" w-full rounded-full bg-slate-200 p-4 text-black focus:bg-white focus:outline-none"
             onChange={handleChange}
-            value={walletAddress}
+            value={walletAddress} 
           />
-        </form>
-        <button className="rounded-full bg-slate-600 px-64 py-4 hover:bg-slate-400">
+          <button type="submit" className="rounded-full w-full bg-slate-600 px-64 py-4 hover:bg-slate-400">
           Request Tokens
         </button>
+        </form>
+        
       </div>
     </div>
     </main>
