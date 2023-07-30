0d62be8785ac497c9bf5e845cfd149bbe509f215
vonhatcuong <vonhatcuongyy@gmail.com>
2023-07-31 00:53:32 +0700

    feat: faucet api

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,28 +1,43 @@
-const {mintSoul, bondSoul} = require('./soulboundController');
-const {mint} = require('./nftController');
+const { mintSoul, bondSoul } = require('./soulboundController');
+const { mint } = require('./nftController');
 const Web3 = require('web3');
+const { checkSoulBoundExist } = require('./soulboundController.js');
+const { transfer } = require('./faucetcontroller.js');
 require('dotenv').config();
 
-const web3 = new Web3("https://rpc.testnet.fantom.network/");
+const web3 = new Web3('https://rpc.testnet.fantom.network/');
 
 async function generateMint(req, res, next) {
-    try {
-        // Mint NFT
-        const mintReceipt = await mint();
-        console.log('Mint Receipt:', mintReceipt);
+  try {
+    // Mint NFT
+    const mintReceipt = await mint();
+    console.log('Mint Receipt:', mintReceipt);
 
-        // Mint Soulbound NFT
-        const soulMintReceipt = await mintSoul();
-        console.log('Soul Mint Receipt:', soulMintReceipt);
+    // Mint Soulbound NFT
+    const soulMintReceipt = await mintSoul();
+    console.log('Soul Mint Receipt:', soulMintReceipt);
 
-        // Bond Soulbound NFT
-        const bondSoulReceipt = await bondSoul(req.params.address); 
-        console.log('Bond Soul Receipt:', bondSoulReceipt);
-        res.json({ message: 'NFT mint and soulbound successfully' });
-        
-    } catch (error) {
-        console.log(error);
-    }
+    // Bond Soulbound NFT
+    const bondSoulReceipt = await bondSoul(req.params.address);
+    console.log('Bond Soul Receipt:', bondSoulReceipt);
+    res.json({ message: 'NFT mint and soulbound successfully' });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-module.exports = {generateMint};
+const faucet = async (req, res) => {
+  try {
+    const isSoubound = checkSoulBoundExist(req.params.address);
+    if (isSoubound) {
+      const faucetReceipt = await transfer(req.params.address);
+      if (faucetReceipt && faucetReceipt.status == true) {
+        res.json({ message: 'faucet successfully' });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { generateMint, faucet };
--- backend/controllers/nftController.js
+++ backend/controllers/nftController.js
@@ -1,71 +1,63 @@
 const Web3 = require('web3');
-const fs = require("fs");
+const fs = require('fs');
 require('dotenv').config();
 
-const web3 = new Web3("https://rpc.testnet.fantom.network/");
+const web3 = new Web3('https://rpc.testnet.fantom.network/');
 const path = require('path');
 
 const filePath = path.join(__dirname, '../abis/nft.json');
 const contractNFTJson = fs.readFileSync(filePath);
 const NFTAbi = JSON.parse(contractNFTJson);
-const {getGasPrice, getNonce} = require('./utils.js');
+const { getGasPrice, getNonce } = require('./utils.js');
 
 const createNFTContract = () => {
-    
-    const contract = new web3.eth.Contract(
-        NFTAbi,
-        process.env.CONTRACT_NFT
-    );
+  const contract = new web3.eth.Contract(NFTAbi, process.env.CONTRACT_NFT);
 
-    return contract;
+  return contract;
 };
 
-
-let token_id = 101;
+let token_id = 1002;
 const mint = async () => {
-    const NFTContract = createNFTContract();
+  const NFTContract = createNFTContract();
 
-    const encodedTransaction = await NFTContract.methods
-        .mint(
-            process.env.ACCOUNT_OWNER,
-            token_id++ // token_id of nft
-        )
-        .encodeABI();
+  const encodedTransaction = await NFTContract.methods
+    .mint(
+      process.env.ACCOUNT_OWNER,
+      token_id++, // token_id of nft
+    )
+    .encodeABI();
 
-    const gas = 480000;
-    const gasPrice = web3.utils.toHex(await getGasPrice());
-    const nonce = web3.utils.toHex(await getNonce());
-    const { address, privateKey } = web3.eth.accounts.privateKeyToAccount(
-        process.env.PRIVATE_KEY
+  const gas = 480000;
+  const gasPrice = web3.utils.toHex(await getGasPrice());
+  const nonce = web3.utils.toHex(await getNonce());
+  const { address, privateKey } = web3.eth.accounts.privateKeyToAccount(
+    process.env.PRIVATE_KEY,
+  );
+
+  let txParams = {
+    from: web3.utils.toChecksumAddress(address),
+    gas,
+    gasPrice,
+    nonce,
+    data: encodedTransaction,
+    to: process.env.CONTRACT_NFT,
+  };
+
+  const signedTransaction = await web3.eth.accounts.signTransaction(
+    txParams,
+    privateKey,
+  );
+
+  return new Promise(async (resolve, reject) => {
+    const receipt = await web3.eth.sendSignedTransaction(
+      signedTransaction.raw || signedTransaction.rawTransaction,
     );
+    if (receipt && receipt.status) {
+      resolve(receipt);
+    } else {
+      reject('Transaction failed to be signed');
+    }
+  });
+};
 
-    let txParams = {
-        from: web3.utils.toChecksumAddress(address),
-        gas,
-        gasPrice,
-        nonce,
-        data: encodedTransaction,
-        to: process.env.CONTRACT_NFT
-    };
-
-    const signedTransaction = await web3.eth.accounts.signTransaction(
-        txParams,
-        privateKey
-    );
-
-    
-    return new Promise(async(resolve, reject) => { 
-        const receipt = await web3.eth.sendSignedTransaction(
-            signedTransaction.raw || signedTransaction.rawTransaction
-        );
-        if(receipt && receipt.status) {
-            resolve(receipt)
-        }else {
-            reject("Transaction failed to be signed")
-        }
-    });
-
-
-}
-
-module.exports = {mint};
+module.exports = { mint };
